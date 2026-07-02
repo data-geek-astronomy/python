df5391fcf5c34e3b54a5a2dba9daf39374da57ca
Aravind Kumar Nalukurthi <analukur@asu.edu>
2026-07-02 20:30:54 +0000

    Add unique paths grid DP solution

---
+++ 117_unique_paths.py
@@ -0,0 +1,9 @@
+def unique_paths(m, n):
+    dp = [[1] * n for _ in range(m)]
+    for r in range(1, m):
+        for c in range(1, n):
+            dp[r][c] = dp[r - 1][c] + dp[r][c - 1]
+    return dp[m - 1][n - 1]
+
+if __name__ == "__main__":
+    print(unique_paths(3, 7))
