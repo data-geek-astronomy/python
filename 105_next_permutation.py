47bc880c493638b172b9501c13c4917455238c35
Aravind Kumar Nalukurthi <analukur@asu.edu>
2026-07-02 20:30:54 +0000

    Add next permutation solution

---
+++ 105_next_permutation.py
@@ -0,0 +1,16 @@
+def next_permutation(nums):
+    n = len(nums)
+    i = n - 2
+    while i >= 0 and nums[i] >= nums[i + 1]:
+        i -= 1
+    if i >= 0:
+        j = n - 1
+        while nums[j] <= nums[i]:
+            j -= 1
+        nums[i], nums[j] = nums[j], nums[i]
+    nums[i + 1:] = reversed(nums[i + 1:])
+    return nums
+
+if __name__ == "__main__":
+    print(next_permutation([1, 2, 3]))
+    print(next_permutation([3, 2, 1]))
