17fdd017355590c427d235f5b6dc0320289ece67
Aravind Kumar Nalukurthi <analukur@asu.edu>
2026-07-02 20:30:54 +0000

    Add search in rotated sorted array

---
+++ 109_search_rotated_sorted_array.py
@@ -0,0 +1,20 @@
+def search(nums, target):
+    lo, hi = 0, len(nums) - 1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        if nums[mid] == target:
+            return mid
+        if nums[lo] <= nums[mid]:
+            if nums[lo] <= target < nums[mid]:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        else:
+            if nums[mid] < target <= nums[hi]:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+    return -1
+
+if __name__ == "__main__":
+    print(search([4, 5, 6, 7, 0, 1, 2], 0))
