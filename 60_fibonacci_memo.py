a891f915fc627cec763ae884753bdfdb753e9dae
Aravind Kumar Nalukurthi <analukur@asu.edu>
2026-07-02 20:03:39 +0000

    Add fibonacci with memoization

---
+++ 60_fibonacci_memo.py
@@ -0,0 +1,12 @@
+def fibonacci_memo(n, memo=None):
+    if memo is None:
+        memo = {}
+    if n in memo:
+        return memo[n]
+    if n <= 1:
+        return n
+    memo[n] = fibonacci_memo(n - 1, memo) + fibonacci_memo(n - 2, memo)
+    return memo[n]
+
+if __name__ == "__main__":
+    print(fibonacci_memo(30))
