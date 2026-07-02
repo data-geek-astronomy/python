fc675211b5fde1cd7332240d172bff4daf6d524f
Aravind Kumar Nalukurthi <analukur@asu.edu>
2026-07-02 20:30:54 +0000

    Add set matrix zeroes solution

---
+++ 108_set_matrix_zeroes.py
@@ -0,0 +1,16 @@
+def set_zeroes(matrix):
+    rows, cols = len(matrix), len(matrix[0])
+    zero_rows, zero_cols = set(), set()
+    for r in range(rows):
+        for c in range(cols):
+            if matrix[r][c] == 0:
+                zero_rows.add(r)
+                zero_cols.add(c)
+    for r in range(rows):
+        for c in range(cols):
+            if r in zero_rows or c in zero_cols:
+                matrix[r][c] = 0
+    return matrix
+
+if __name__ == "__main__":
+    print(set_zeroes([[1, 1, 1], [1, 0, 1], [1, 1, 1]]))
