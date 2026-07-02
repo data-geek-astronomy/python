549cb78ae42ae92fd5301cd614044e2bebfa795f
Aravind Kumar Nalukurthi <analukur@asu.edu>
2026-07-02 20:29:17 +0000

    Add union-find disjoint set implementation

---
+++ 82_union_find.py
@@ -0,0 +1,26 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        root_x, root_y = self.find(x), self.find(y)
+        if root_x == root_y:
+            return False
+        if self.rank[root_x] < self.rank[root_y]:
+            root_x, root_y = root_y, root_x
+        self.parent[root_y] = root_x
+        if self.rank[root_x] == self.rank[root_y]:
+            self.rank[root_x] += 1
+        return True
+
+if __name__ == "__main__":
+    uf = UnionFind(5)
+    uf.union(0, 1)
+    uf.union(1, 2)
+    print(uf.find(0) == uf.find(2), uf.find(0) == uf.find(4))
