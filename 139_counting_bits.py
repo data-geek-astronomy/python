6020c043b06e9653477dce41f6e5c1b36430814d
Aravind Kumar Nalukurthi <analukur@asu.edu>
2026-07-05 04:21:52 +0000

    Add counting bits for range 0..n

---
+++ 139_counting_bits.py
@@ -0,0 +1,8 @@
+def count_bits(n):
+    dp = [0] * (n + 1)
+    for i in range(1, n + 1):
+        dp[i] = dp[i >> 1] + (i & 1)
+    return dp
+
+if __name__ == "__main__":
+    print(count_bits(5))
