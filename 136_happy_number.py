8ac74834539a213f6db992cdff4afcf41f74ffa6
Aravind Kumar Nalukurthi <analukur@asu.edu>
2026-07-05 04:21:52 +0000

    Add happy number checker

---
+++ 136_happy_number.py
@@ -0,0 +1,10 @@
+def is_happy(n):
+    seen = set()
+    while n != 1 and n not in seen:
+        seen.add(n)
+        n = sum(int(d) ** 2 for d in str(n))
+    return n == 1
+
+if __name__ == "__main__":
+    print(is_happy(19))
+    print(is_happy(2))
